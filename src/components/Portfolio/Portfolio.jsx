29d60223c5532b2580c2bcc6f2c560b97ce4af16
akshaySadaphal <akshaysadaphal67@gmail.com>
2023-12-08 09:40:15 +0530

    All UI Changes And Functional Changes

---
+++ src/components/Portfolio/Portfolio.jsx
@@ -0,0 +1,23 @@
+import React, { useContext } from "react";
+import "./Portfolio.css";
+import { Swiper, SwiperSlide } from "swiper/react"
+import "swiper/css";
+import Sidebar from "../../img/sidebar.png";
+import Ecommerce from "../../img/ecommerce.png";
+import HOC from "../../img/hoc.png";
+import MusicApp from "../../img/musicapp.png";
+import { themeContext } from "../../Context";
+const Portfolio = () => {
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+  return (
+    <div className="portfolio" id="portfolio">
+      {/* heading */}
+      <span style={{color: darkMode?'white': ''}}>Recent Projects</span>
+               <span>Portfolio</span>
+               <b>Work in Progress</b>    
+    </div>
+  );
+};
+
+export default Portfolio;
